eb577dbeb93877049e4fc7b9f39217b47eea49d8
matheus braz <matheusbraz871@outlook.com>
2023-06-04 17:39:42 -0300

    fiz o segundo execicio

---
+++ fundamentos/secao3/dia1/exercicio1.js
@@ -0,0 +1,8 @@
+let num1 = 100;
+let num2 = 50;
+console.log('Adição ' + (num1 + num2));
+console.log('Subtração ' + (num1 - num2));
+console.log('Multiplicação ' + (num1 * num2));
+console.log('Divisao ' + (num1 / num2));
+console.log('Resto ' + (num1 % num2));
+
---
+++ fundamentos/secao3/dia1/exercicios2.js
@@ -0,0 +1,10 @@
+let num1 = 100;
+let num2 = 50;
+
+if (num1 > num2) {
+    console.log('o ' + num1 + ' é maior que o ' + num2);
+} else if (num1 === num2) {
+    console.log('os numeros são iguais');
+} else {
+    console.log(num1 + ' é menor que o ' + num2);
+}

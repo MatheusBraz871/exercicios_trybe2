f2d9afb5f3c5d3f03205c4d6123102fa27b46af7
matheus braz <matheusbraz871@outlook.com>
2023-06-04 17:49:06 -0300

    fiz o exercicio 3 do dia 1

---
+++ fundamentos/secao3/dia1/exercicios3.js
@@ -0,0 +1,13 @@
+let num1 = 300;
+let num2 = 200;
+let num3 = 100;
+
+if (num1 > num2 && num1 > num3) {
+    console.log(num1 + ' é maior que ' + num2 + ' e ' + num3);
+} else if (num2 > num1 && num2 > num3) {
+    console.log(num2 + ' é maior que ' + num1 + ' e ' + num3);
+} else if (num3 > num1 && num3 > num2) {
+    console.log(num3 + ' é maior que ' + num1 + ' e ' + num2)
+} else {
+    console.log('Os numeros são iguais')
+}

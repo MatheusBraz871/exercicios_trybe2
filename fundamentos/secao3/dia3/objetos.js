2b8af835d011c14367a470aa63850b9e2e20d1c5
matheus braz <matheusbraz871@outlook.com>
2023-06-05 20:40:33 -0300

    começando a estudar objetos

---
+++ fundamentos/secao3/dia3/objetos.js
@@ -0,0 +1,16 @@
+const user = {
+    id: 99,
+    email: 'jakeperalta@gmail.com',
+    info: {
+      name: 'Jake',
+      lastName: 'Peralta',
+      address: {
+        street: 'Rua Dalvo Trombeta, 357 - Fundos',
+        district: 'Xererê',
+        city: 'Santana do Livramento',
+        state: 'Rio Grande do Norte',
+      },
+    },
+  };
+  
+ console.log(user.info.address.city)

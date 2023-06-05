c2e10f234bc86271875cb0a43da5c939e8385a8f
matheus braz <matheusbraz871@outlook.com>
2023-06-05 15:44:24 -0300

    comecei meus estudos em funcoes

---
+++ fundamentos/secao3/dia3/funções.js
@@ -0,0 +1,12 @@
+function giveMeAHug() {
+    return 'Voce quer um abraço ?';
+}
+
+console.log(giveMeAHug()); 
+
+function sum(num1, num2) {
+    return num1 + num2;
+  };
+  
+  console.log(sum(10, 8)); 
+  console.log(sum(58000, 27547));

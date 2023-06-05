ca960f05ea535eada3aa8642d5d5677c779d394f
matheus braz <matheusbraz871@outlook.com>
2023-06-05 15:45:49 -0300

    pratiquei um pouco de funcoes

---
+++ fundamentos/secao3/dia3/pratica-funcoes.js
@@ -0,0 +1,17 @@
+const balance = 100;
+
+function sumBalance(value) {
+    return balance + value;
+}
+
+function subBalance(value) {
+    return balance - value;
+}
+
+function multBalance(value) {
+    return balance * value;
+}
+
+function divBalance(value) {
+    return balance / value;
+}

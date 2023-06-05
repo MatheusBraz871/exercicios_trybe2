a6eff97764ccdd53d521fa08a76864622537a1ee
matheus braz <matheusbraz871@outlook.com>
2023-06-05 10:46:43 -0300

    estudei estrutura de repetição for

---
+++ fundamentos/secao3/dia1/for.js
@@ -0,0 +1,14 @@
+const laps = 68;
+
+for (let index = 1; index <= laps; index += 1) {
+  console.log(`Volta ${index} completada!`);
+}
+
+console.log('Acabou a corrida!');
+
+
+const ano = 12;
+for (let index = 1; index <= ano; index += 1) {
+    console.log(`mes ${index}`);
+} 
+console.log('acabou o ano');

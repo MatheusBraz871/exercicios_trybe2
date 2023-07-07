ec1a8ed217c2cfde3a1e389d28fe793bf571d1b2
matheus braz <matheusbraz871@outlook.com>
2023-07-07 17:57:45 -0300

    criando e adicionando novos elementos

--- fundamentos/secao4/day2/scriptclass.js
+++ fundamentos/secao4/day2/scriptclass.js
@@ -1,6 +1,16 @@
 const firstParent = document.getElementById('first');
-let newElement = document.createElement('div');
-newElement.classList.add('circle');
-newElement.classList.add('small');
-newElement.classList.add('red');
-firstParent.appendChild(newElement);
+
+let newDiv = document.createElement('div');
+newDiv.classList.add('circle');
+newDiv.classList.add('small');
+newDiv.classList.add('green');
+
+let newDiv1 = document.createElement('div');
+newDiv1.className = 'circle small yellow';
+
+let newDiv2 = document.createElement('div');
+newDiv2.className = 'circle small red';
+
+firstParent.appendChild(newDiv);
+firstParent.appendChild(newDiv1);
+firstParent.appendChild(newDiv2);

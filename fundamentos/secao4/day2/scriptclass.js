4d9a4706e38a9df428836a9102d0a243d60e0692
matheus braz <matheusbraz871@outlook.com>
2023-07-07 18:48:20 -0300

    criando elementos atraves de funções

--- fundamentos/secao4/day2/scriptclass.js
+++ fundamentos/secao4/day2/scriptclass.js
@@ -1,16 +1,23 @@
 const firstParent = document.getElementById('first');
 
-let newDiv = document.createElement('div');
-newDiv.classList.add('circle');
-newDiv.classList.add('small');
-newDiv.classList.add('green');
+const createDiv = (classElements, element) => {
+    const newDiv = document.createElement(element);
+    newDiv.className = classElements;
+    return newDiv;
+};
 
-let newDiv1 = document.createElement('div');
-newDiv1.className = 'circle small yellow';
+firstParent.appendChild(createDiv('small circle green', 'div'));
 
-let newDiv2 = document.createElement('div');
-newDiv2.className = 'circle small red';
+//let newDiv = document.createElement('div'); 
+//newDiv.classList.add('circle');
+//newDiv.classList.add('small');
+//newDiv.classList.add('green');
 
-firstParent.appendChild(newDiv);
-firstParent.appendChild(newDiv1);
-firstParent.appendChild(newDiv2);
+//let newDiv1 = document.createElement('div');
+//newDiv1.className = 'circle small yellow';
+
+//let newDiv2 = document.createElement('div');
+//newDiv2.className = 'circle small red';
+
+//firstParent.appendChild(newDiv1);
+//firstParent.appendChild(newDiv2);

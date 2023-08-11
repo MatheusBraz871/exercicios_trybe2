8942261bb33edc4160144b42a2f849eaa3f4526a
Matheus <matheusbraz871@outlook.com>
2023-08-10 23:22:00 -0300

    the second requiriment

--- Fudamentos/secao5/dia 1/exercicios do dia 1/script.js
+++ Fudamentos/secao5/dia 1/exercicios do dia 1/script.js
@@ -2,7 +2,12 @@ const inputText = document.querySelector("#input-text");
 const inputCheckbox = document.querySelector("#input-checkbox");
 const hrefLink = document.querySelector("#href");
 
-function comportamento(acontecimento) {
+function comportamento1(acontecimento) {
    acontecimento.preventDefault();
 }
-hrefLink.addEventListener('click', comportamento);
+hrefLink.addEventListener('click', comportamento1);
+
+function comportamneto2(acontecimento) {
+    acontecimento.preventDefault();
+}
+inputCheckbox.addEventListener('click', comportamneto2);
